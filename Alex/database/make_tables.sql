b01b043ac123f83b676448b6b4ac7099d8daed54
Alex <wolf1in1forest@gmail.com>
2024-05-05 18:06:55 +0300

    update make tables

--- Alex/database/make_tables.sql
+++ Alex/database/make_tables.sql
@@ -38,14 +38,11 @@ CREATE TABLE saved (
 CREATE TABLE complaint_image (
     id_user BIGINT DEFAULT NULL,
     id_image BIGINT NOT NULL,
-    id_image_author BIGINT NOT NULL,
-    CONSTRAINT fk_id_author
-        FOREIGN KEY(id_image_author) REFERENCES "user"(id) ON DELETE CASCADE,
     CONSTRAINT fk_id_user
         FOREIGN KEY(id_user) REFERENCES "user"(id) ON DELETE SET NULL,
     CONSTRAINT fk_id_image
         FOREIGN KEY(id_image) REFERENCES image(id) ON DELETE CASCADE,
-    CONSTRAINT pk_author_user_object PRIMARY KEY (id_user, id_image, id_image_author)
+    CONSTRAINT pk_author_user_object PRIMARY KEY (id_user, id_image)
 );
 
 CREATE TABLE complaint_profile (
@@ -61,12 +58,9 @@ CREATE TABLE complaint_profile (
 CREATE TABLE complaint_comment (
     id_user BIGINT DEFAULT NULL,
     id_comment BIGINT NOT NULL,
-    id_comment_author BIGINT NOT NULL,
-    CONSTRAINT fk_id_author
-        FOREIGN KEY(id_comment_author) REFERENCES "user"(id) ON DELETE CASCADE,
     CONSTRAINT fk_id_user
         FOREIGN KEY(id_user) REFERENCES "user"(id) ON DELETE SET NULL,
     CONSTRAINT fk_id_comment
         FOREIGN KEY(id_comment) REFERENCES comment(id) ON DELETE CASCADE,
-    CONSTRAINT pk_author_user_comment PRIMARY KEY (id_user, id_comment, id_comment_author)
+    CONSTRAINT pk_author_user_comment PRIMARY KEY (id_user, id_comment)
 );
--- project/database/make_tables.sql
+++ project/database/make_tables.sql
@@ -16,6 +16,7 @@ CREATE TABLE "user" (
     amount_of_complaints_on_image INT CHECK (amount_of_complaints_on_image >= 0) DEFAULT 0
 );
 
+
 CREATE TABLE image (
     id BIGSERIAL PRIMARY KEY,
     author_id BIGINT NOT NULL,
@@ -32,24 +33,6 @@ CREATE TABLE image (
     CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES "user"(id)
 );
 
-CREATE TABLE image_tag (
-    image_id BIGINT,
-    tag_id INT NOT NULL,
-    CONSTRAINT fk_image_tag_to_image FOREIGN KEY (image_id) REFERENCES image(id),
-    CONSTRAINT fk_image_tage_to_tag FOREIGN KEY (tag_id) REFERENCES tag(id),
-    CONSTRAINT pk_image_tag PRIMARY KEY (image_id, tag_id)
-);
-
-CREATE TABLE comment (
-    id BIGSERIAL PRIMARY KEY,
-    description TEXT DEFAULT NULL,
-    image_id BIGINT NOT NULL,
-    user_id BIGINT NOT NULL,
-    report_counter INT DEFAULT 0,
-    adding_date DATE NOT NULL,
-    CONSTRAINT fk_comment_to_user FOREIGN KEY (user_id) REFERENCES "user"(id),
-    CONSTRAINT fk_comment_to_image FOREIGN KEY (image_id) REFERENCES image(id)
-);
 
 CREATE TABLE "like" (
     id_user BIGINT NOT NULL,
@@ -78,14 +61,11 @@ CREATE TABLE saved (
 CREATE TABLE complaint_image (
     id_user BIGINT DEFAULT NULL,
     id_image BIGINT NOT NULL,
-    id_image_author BIGINT NOT NULL,
-    CONSTRAINT fk_id_author
-        FOREIGN KEY(id_image_author) REFERENCES "user"(id) ON DELETE CASCADE,
     CONSTRAINT fk_id_user
         FOREIGN KEY(id_user) REFERENCES "user"(id) ON DELETE SET NULL,
     CONSTRAINT fk_id_image
         FOREIGN KEY(id_image) REFERENCES image(id) ON DELETE CASCADE,
-    CONSTRAINT pk_author_user_object PRIMARY KEY (id_user, id_image, id_image_author)
+    CONSTRAINT pk_author_user_object PRIMARY KEY (id_user, id_image)
 );
 
 CREATE TABLE complaint_profile (
@@ -98,15 +78,32 @@ CREATE TABLE complaint_profile (
     CONSTRAINT pk_profile_user PRIMARY KEY (id_user, id_profile_owner)
 );
 
+CREATE TABLE comment (
+    id BIGSERIAL PRIMARY KEY,
+    description TEXT DEFAULT NULL,
+    image_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    report_counter INT DEFAULT 0,
+    adding_date DATE NOT NULL,
+    CONSTRAINT fk_comment_to_user FOREIGN KEY (user_id) REFERENCES "user"(id),
+    CONSTRAINT fk_comment_to_image FOREIGN KEY (image_id) REFERENCES image(id)
+);
+
 CREATE TABLE complaint_comment (
     id_user BIGINT DEFAULT NULL,
     id_comment BIGINT NOT NULL,
-    id_comment_author BIGINT NOT NULL,
-    CONSTRAINT fk_id_author
-        FOREIGN KEY(id_comment_author) REFERENCES "user"(id) ON DELETE CASCADE,
     CONSTRAINT fk_id_user
         FOREIGN KEY(id_user) REFERENCES "user"(id) ON DELETE SET NULL,
     CONSTRAINT fk_id_comment
         FOREIGN KEY(id_comment) REFERENCES comment(id) ON DELETE CASCADE,
-    CONSTRAINT pk_author_user_comment PRIMARY KEY (id_user, id_comment, id_comment_author)
+    CONSTRAINT pk_author_user_comment PRIMARY KEY (id_user, id_comment)
+);
+
+
+CREATE TABLE image_tag (
+    image_id BIGINT,
+    tag_id INT NOT NULL,
+    CONSTRAINT fk_image_tag_to_image FOREIGN KEY (image_id) REFERENCES image(id),
+    CONSTRAINT fk_image_tage_to_tag FOREIGN KEY (tag_id) REFERENCES tag(id),
+    CONSTRAINT pk_image_tag PRIMARY KEY (image_id, tag_id)
 );
