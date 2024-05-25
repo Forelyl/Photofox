5b5d5a112c4598fbc7f3afdb17f287303e780b27
Naz <ngvoznyuk@gmail.com>
2024-05-25 23:04:09 +0300

    Lite changes to api names and sign page update

--- frontend/PhotoFox_front/src/App.jsx
+++ frontend/PhotoFox_front/src/App.jsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import RootLayout from "./pages/RootLayout.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
-import SignPage from "./pages/SignPage/SignPage.jsx";
+import SignPage, { action as authAction } from "./pages/SignPage/SignPage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
 import ProfileEdit from "./pages/ProfileEdit.jsx";
 import ProfilePictures from "./pages/ProfilePictures.jsx";
@@ -13,7 +13,7 @@ import { loaderCheckToken, tokenLoader } from "./utils/auth.js";
 const router = createBrowserRouter([
     { path: '/', id: 'root', element: <RootLayout/>, children: [
         { index: true, element: <HomePage /> },
-        { path: 'sing', element: <SignPage /> },
+        { path: 'sing?mode=in', element: <SignPage />, action: authAction },
         { path: ':profileName', children: [ //TODO add loader that checks if logined and showes editing tools
             { index: true, element: <ProfilePage /> },
             { path: 'edit', element: <ProfileEdit />, loader: loaderCheckToken },
