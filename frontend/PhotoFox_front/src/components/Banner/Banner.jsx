7cf1ddb3774f2c98a25825f6c0c7038444b142de
Alex <wolf1in1forest@gmail.com>
2024-05-25 20:11:26 +0300

    clearing

--- frontend/PhotoFox_front/src/components/Banner/Banner.jsx
+++ frontend/PhotoFox_front/src/components/Banner/Banner.jsx
@@ -35,10 +35,3 @@ export default function Banner() {
         </div>
     );
 }
-
-//\
-// if (width / height >= 1.6) document.getElementById('background').style.backgroundImage = "url('/public/logo_background.svg')";
-// else                       {
-//     document.getElementById('background').style.backgroundImage = "url('/public/logo_background1.svg')";
-//     document.getElementById('background').style.backgroundSize = "200%"
-//     document.getElementById('background').style.backgroundPositionX = "center";
