ead422ced3387185239d8405695e1dfa7c05a7c9
Alex <wolf1in1forest@gmail.com>
2024-05-27 17:32:53 +0300

    dropdown

--- frontend/PhotoFox_front/src/components/Menu/DropdownMenu.jsx
+++ frontend/PhotoFox_front/src/components/Menu/DropdownMenu.jsx
@@ -11,41 +11,41 @@ export default function DropdownMenu({ onClose, className }) {
             {!token && (
                 <Link to='/sign?mode=in'>
                     <img src='/DropdownElements/log_in.svg' alt='Log in'/>
-                    <p>Log in</p>
+                    <span>Log in</span>
                 </Link>
             )}
             {token && (
                 <>
                     <Link to={`/${name}`}>
                         <img src='/DropdownElements/profile.svg' alt='Profile'/>
-                        <p>Profile</p>
+                        <span>Profile</span>
                     </Link>
                     <Link to={`/${name}/pictures/saved`}>
                         <img src='/DropdownElements/saved.svg' alt='Saved pictures'/>
-                        <p>Saved</p>
+                        <span>Saved</span>
                     </Link>
                     <Link to={`/${name}/pictures/published`}>
                         <img src='/DropdownElements/published.svg' alt='Published pictures'/>
-                        <p>Published</p>
+                        <span>Published</span>
                     </Link>
                     <button onClick={() => logOut(onClose, navigate)}>
                         <img src='/DropdownElements/log_out.svg' alt='Log out'/>
-                        <p>Log out</p>
+                        <span>Log out</span>
                     </button>
                 </>
             )}
             <hr/>
             <Link to='info?type=terms'>
                 <img src='/DropdownElements/terms_of_service.svg' alt='Terms of service'/>
-                <p>Terms of Service</p>
+                <span>Terms of Service</span>
             </Link>
             <Link to='info?type=privacy'>
                 <img src='/DropdownElements/privacy_policy.svg' alt='Privacy policy'/>
-                <p>Privacy policy</p>
+                <span>Privacy policy</span>
             </Link>
             <Link to='info?type=about'>
                 <img src='/DropdownElements/about_us.svg' alt='About us'/>
-                <p>About us</p>
+                <span>About us</span>
             </Link>
         </div>
     );
