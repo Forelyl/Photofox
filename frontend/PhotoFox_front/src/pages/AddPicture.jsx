d90c5c0a9f5689ef765daf5948a1f7eeff0400bc
Naz <ngvoznyuk@gmail.com>
2024-05-28 17:39:43 +0300

    started

--- frontend/PhotoFox_front/src/pages/AddPicture.jsx
+++ frontend/PhotoFox_front/src/pages/AddPicture.jsx
@@ -1,8 +1,23 @@
+import './AddPicture.css'
+import NavBar from "../../components/Menu/NavBar.jsx";
+import { useState } from "react";
 
 export default function AddPicture() {
     return (
         <>
-            <h1>Hiq1</h1>
+            <NavBar hideAdd={true} hideSearch={true}/>
+            <div>
+                <div>
+                    <input type="file" accept="image/*" name='image'/>
+                    <div>
+                        <input type="checkbox" name='download-permission' id='download-permission'/>
+                        <label htmlFor='download-permission'>Allow downloading</label>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </>
     );
 }
