1544dd435c905e3a761539b60bf79cd02a0d3570
Naz <ngvoznyuk@gmail.com>
2024-05-28 15:53:03 +0300

    finished infinite scroll logic

--- backend/API.py
+++ backend/API.py
@@ -216,7 +216,6 @@ async def delete_tag(tag_id: Annotated[int, Header(ge=1)]):
 #============================================
 @app.get('/image/last', tags=['image'], response_model=list[DB_Returns.Image_PC])
 async def get_last_images_pc(last_image_id: int = -1):
-    print(last_image_id)
     return await db.get_images_pc(last_image_id)
 
 @app.get('/image/last/mobile', tags=['image'], response_model=list[DB_Returns.Image_mobile])
